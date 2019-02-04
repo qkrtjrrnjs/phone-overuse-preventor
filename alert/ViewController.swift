516274141cbd566c0f6c727a3952d60af38f024e
Seok Kwun Park <park960@purdue.edu>
2019-02-04 18:23:06 -0500

    create reusable popup view and refactor them into storyboards

--- alert/ViewController.swift
+++ alert/ViewController.swift
@@ -48,10 +48,9 @@ class ViewController: UIViewController {
         if menuPressed == 0{
             //expand buttons
             UIView.animate(withDuration: 0.3, animations: {
-                //change alpha
-                self.play.alpha = 1
-                self.time.alpha = 1
-                self.unlock.alpha = 1
+                
+                //change alpha value to 1
+                self.setAlpha(alphaValue: 1)
                 
                 //animations
                 self.play.center = self.playButtonCenter
@@ -67,10 +66,9 @@ class ViewController: UIViewController {
         else{
             //collapse buttons
             UIView.animate(withDuration: 0.3, animations: {
-                //change alpha
-                self.play.alpha = 0
-                self.time.alpha = 0
-                self.unlock.alpha = 0
+                
+                //change alpha value to 0
+                self.setAlpha(alphaValue: 0)
                 
                 //animations
                 self.play.center = self.menu.center
@@ -85,8 +83,14 @@ class ViewController: UIViewController {
         }
     }//closing bracket of menu pressed
     
+    func setAlpha(alphaValue: Int){
+        play.alpha = 0
+        time.alpha = 0
+        unlock.alpha = 0
+    }
+    
     @IBAction func timeButton(_ sender: UIButton) {
-        askForInput(title: "Time", message: "Enter ", defaultText: "Enter a number")
+
     }
     
     
@@ -95,27 +99,8 @@ class ViewController: UIViewController {
     
     
     @IBAction func unlockButton(_ sender: Any) {
-        askForInput(title: "Unlocks", message: "Enter maximum number of unlocks for today", defaultText: "Enter a number")
+        
     }
     
-    func askForInput(title: String, message: String, defaultText: String){
-        //1. Create the alert controller.
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        
-        //2. Add the text field. You can configure it however you need.
-        alert.addTextField { (textField) in
-            textField.keyboardType = UIKeyboardType.numberPad
-            textField.placeholder = defaultText
-        }
-        
-        // 3. Grab the value from the text field, and print it when the user clicks OK.
-        alert.addAction(UIAlertAction(title: "Submit", style: .default, handler: { [weak alert] (_) in
-            let textField = alert?.textFields![0] // Force unwrapping because we know it exists.
-            self.hour = Int((textField?.text)!)!
-        }))
-        
-        // 4. Present the alert.
-        self.present(alert, animated: true, completion: nil)
-    }
 }
 
